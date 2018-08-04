360f72be02a328268a5dee620980eca7bd0c09e9
Tschiboka <tibi.aki.tivadar@gmail.com>
2018-08-04 13:55:42 +0100

    letters added on pencils and palette icon triggers color dialog box

--- crayons.js
+++ crayons.js
@@ -1,8 +1,7 @@
+var drawingColor = "black";
 
-const box = document.getElementById("container");
-box.addEventListener("click", (event) => cursorIsOnSheet(event.clientX,event.clientY) );
 addPencilMouseListeners();
-
+addColorPaletteMouseListener();
 
 function addPencilMouseListeners() {
     const colors = ["red", "pink", "orange", "yellow", "purple", "green", "blue", "brown", "white", "gray", "black"];
@@ -10,22 +9,31 @@ function addPencilMouseListeners() {
     colors.map((color) => {
         const pseudo = document.getElementById(`${color}-pencil-pseudo`),
               element = document.getElementById(`pencil-${color}`);
-        pseudo.addEventListener("mouseenter", (event) => { 
+        pseudo.addEventListener("mouseenter", () => { 
             element.style.animation = "pencil-move-out 0.3s linear";
             element.style.WebkitAnimation = "pencil-move-out 0.3s linear";
             element.style.left = "-90px";
         });  // end of mouseenter
-        pseudo.addEventListener("mouseout", (event) => { 
+        pseudo.addEventListener("mouseout", () => { 
             element.style.animation = "pencil-move-in 0.6s linear"; 
             element.style.WebkitAnimation = "pencil-move-in 0.6s linear"; 
             element.style.left = "-60px";
-        }); // end of mouseout       
+        }); // end of mouseout    
+        pseudo.addEventListener("click", ()=> {
+            drawingColor = color;
+            document.getElementById("color-display").style.background = color;
+            console.log(drawingColor);
+        }); // end of click 
     }); // end of color mapping
-}
+} // end of addPencilMouseListener
 
-const cursorIsOnSheet = (x,y) => {
-    const sheet = document.getElementById("sheet"),
-          sheetStyle = window.getComputedStyle(sheet);    
-    console.log(x,y, sheetStyle);
-};
+function addColorPaletteMouseListener() {
+    const palette = document.getElementById("palette-icon");
+    palette.addEventListener("click", () => {
+        console.log("click");
+        const dialog = document.getElementById("color-selector-dialog-box");
+        dialog.click();
+    });
+} // end of addColorPaletteMouseListener
+
 
