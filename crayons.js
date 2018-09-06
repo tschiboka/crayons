9c25706b30f95bdb2413e79244e8d5b68c737d2e
Tschiboka <tibi.aki.tivadar@gmail.com>
2018-09-06 14:05:04 +0100

    octagon added

--- crayons.js
+++ crayons.js
@@ -1512,6 +1512,7 @@ function setPolygon() {
         case 5: { addPositioner(workTop, 5, [[40,120], [180,20], [340,120], [280,280], [80,280]]); break; }
         case 6: { addPositioner(workTop, 6, [[115,20], [265,20], [320,150], [265,280], [115,280], [60,150]]); break; }
         case 7: { addPositioner(workTop, 7, [[60,181], [80,72], [180,20], [285,72], [320,181], [260,280], [130,280]]); break; }
+        case 8: { addPositioner(workTop, 8, [[110,20], [270,20], [320,90], [320,210], [270,280], [110,280], [60,210], [60,90]]); break; }
     } // end of switch edges
     //addPositioner(workTop, 3, [[180,150], [80,150], [280,140]]);
 
